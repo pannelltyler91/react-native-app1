4d908a6743ba3b121b16e733d734d5c60ae5e77a
Tyler <pannelltyler91@gmail.com>
2024-07-22 07:21:21 +0900

    adding google vision api to english page

--- app/(tabs)/english.jsx
+++ app/(tabs)/english.jsx
@@ -1,14 +1,34 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text,TouchableOpacity,Image} from 'react-native';
+import React, { useState } from 'react';
 import { Card } from '@rneui/themed';
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 
 const English = () =>{
+    const [imageUri, setImageUri] = useState();
+    const [labels, setLabels] = useState([]);
+
+    const pickImage = async () =>{
+        try{
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes:ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [4,3],
+                quality:1,
+           })
+           if(!result.canceled){
+            setImageUri(result.assets[0].uri)
+           }
+           console.log(result);
+        } catch(err) {
+            console.log(err, "Error picking Image!")
+        }
+    }
+
     return(
         <View className="flex-1 items-center justify-center bg-third">
-            <Card>
-                <Card.Title>English</Card.Title>
-            </Card>
+            <Text>English</Text>
             
         </View>
     )
