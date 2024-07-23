3eedc9cefaa39ff7298355c4540e6103e3bc28f0
Tyler <pannelltyler91@gmail.com>
2024-07-24 08:31:20 +0900

    working on English functionality for Google Vision, getting error

--- app/(tabs)/english.jsx
+++ app/(tabs)/english.jsx
@@ -25,11 +25,102 @@ const English = () =>{
             console.log(err, "Error picking Image!")
         }
     }
-
+    const analyzeImage = async()=>{
+        try{
+            if(!imageUri){
+                alert('Please select and image first!!')
+                return;
+            }
+            const apiKey = 'AIzaSyByoef0PnjP9yDcfWBlmzVI30IiF3pqUBA';
+            const apiURL = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+            //read the image file from local URI and conver it to base64
+            const base64ImageData = await FileSystem.readAsStringAsync(imageUri,{
+                encoding:FileSystem.EncodingType.Base64,
+            })
+        
+        const requestData = {
+            requests:[
+                {
+                    image:{
+                        content:base64ImageData,
+                    },
+                    features:[{type:'LABEL_DETECTION',maxResults: 5}],
+                },
+            ],
+        };
+        const apiResponse = await fetch(apiURL,requestData)
+        console.log(apiResponse._bodyBlob._data)
+        setLabels(apiResponse._data.responses[0].labelAnnotations)
+        }catch(err){
+            console.log('Error analyzing image:',err);
+            alert('Error analyzing image. Please try again later');
+        }
+    }
     return(
         <View className="flex-1 items-center justify-center bg-third">
-            <Text>English</Text>
-            
+            <Text className='text-30 font-bold mb-50 mt-100  '>
+                English
+            </Text>
+            {imageUri &&(
+                <Image
+                source={{uri:imageUri}}
+                style={{width: 300, height: 300}}
+                />
+            )}
+            <TouchableOpacity
+            onPress={pickImage}
+            style={{
+                backgroundColor:'#DDDDDD',
+                padding:10,
+                marginBottom:10,
+                marginTop:20,
+
+            }}>
+                <Text style={{
+                    fontSize:20,
+                    fontWeight:'bold'
+                }}>Choose an image...</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={analyzeImage}
+            >
+                <Text>Analyze Image</Text>
+            </TouchableOpacity>
+            {
+                labels.length > 0 && (
+                    <View>
+                        <Text style={{
+                            fontSize:20,
+                            fontWeight:'bold',
+                            marginTop:20,
+                        }}>
+                            Labels:
+                        </Text>
+                        
+                        {    
+                            labels.map((label) =>(
+                                <Text
+                                    key={label.mid}
+                                    style={{
+                                        fontSize:18,
+                                        marginBottom:10,
+                                    }}                                
+                                >
+                                    {label.description}
+                                </Text>
+                            ))
+
+
+
+
+
+
+
+
+                        }
+                    </View>
+                )
+            }
         </View>
     )
 }
